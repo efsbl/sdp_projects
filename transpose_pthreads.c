8eb6ada1489cafb6be96e9a9901b3875c17fb3d5
efsbl <efsabelli@gmail.com>
2020-06-08 01:16:52 -0300

    newer fixes

--- transpose_pthreads.c
+++ transpose_pthreads.c
@@ -2,21 +2,35 @@
 #include<stdlib.h>
 #include<pthread.h>
 
+void initMatrix(double*, int);
 void printMatrix(double* , int);
 
 double *A, *At, *C;
 int N, T;
+int mode = 0; // mode = 0 -> transpose ; mode = 1 -> multiply 
 
 void *funcion(void *arg){
-	int i, j;
+	int i, j, k;
 	int tid = *(int*)arg;
 	int desde = tid*N/T;
 	int hasta = (tid+1)*N/T;
-	for(i=desde; i<hasta; i++){
-		for(j=0; j<N; j++){
-			At[j*N+i] = A[i*N+j];
+	if(!mode){
+		for(i=desde; i<hasta; i++){
+			for(j=0; j<N; j++){
+				At[j*N+i] = A[i*N+j];
+			}
+		}	
+	}else{
+		for(i=desde; i<hasta; i++){
+			for(j=0; j<N; j++){
+				C[i*N+j] = 0;
+				for(k=0; k<N; k++){
+					C[i*N+j] = C[i*N+j] + A[i*N+k] * At[k+j*N];
+				}
+			}
 		}
 	}
+	
 	pthread_exit(NULL);
 }
 
@@ -24,6 +38,7 @@ void *funcion(void *arg){
 int main(int argc, char*argv[]){
 
 	int i, j, k;
+	int check = 1;
 
 	if (argc < 3){
    		printf("\n Faltan argumentos:: N dimension de la matriz, T cantidad de threads \n");
@@ -34,27 +49,13 @@ int main(int argc, char*argv[]){
   	T = atoi(argv[2]);
 
 	A=(double*)malloc(sizeof(double)*N*N);
-	At=(double*)malloc(sizeof(double)*N*N);\
+	At=(double*)malloc(sizeof(double)*N*N);
+	C=(double*)malloc(sizeof(double)*N*N);
 
 	pthread_t misThreads[T];
 	int threads_ids[T];
 
-	//Inicializacion matrix diagonal inferior
-	/*for(i=0;i<N;i++){
-   		for(j=0;j<N;j++){
-			if (i>=j)
-				A[i*N+j]= 1.0;
-			else
-				A[i*N+j]= 0.0;
-   		}
-  	}*/
-
-	int value = 0;
-  	for(i=0;i<N;i++){
-   		for(j=0;j<N;j++){
-			A[i*N+j] = value++;
-   		}
-  	}
+	initMatrix(A, N);
 
   	printf("\n");
   	printMatrix(A, N);
@@ -66,13 +67,42 @@ int main(int argc, char*argv[]){
         pthread_create(&misThreads[id], NULL, &funcion, (void*)&threads_ids[id]);
     }
 
-    printMatrix(At, N);
+    //Trasposicion
+	for(int id=0; id<T; id++){
+		pthread_join(misThreads[id], NULL);
+	}
 
+	printMatrix(A, N);
+	printf("\n\n");
+	printMatrix(At, N);
 
+	mode = 1;
+	//Multiplicacion
+	
+	for(int id=0; id<T; id++){
+		pthread_join(misThreads[id], NULL);
+	}
+
+	printf("\n\n");
+
+	if(check)
+		printf("Multiplicacion de matrices resultado correcto\n");
+	else
+		printf("Multiplicacion de matrices resultado erroneo\n");
 
   	free(A);
   	free(At);
+  	free(C);
 	return 0;
+}
+
+void initMatrix(double* A, int N){
+	int i, j;
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			A[i*N+j] = 1.0;
+		}
+	}
 }
 
 void printMatrix(double* A, int N){
@@ -84,3 +114,5 @@ void printMatrix(double* A, int N){
 		printf("\n");
 	}
 }
+
+
